2e53cb4f386b7f0aea4139260090f9a3e90a3a1d
Barnuss09 <hollbarna@gmail.com>
2020-02-18 16:37:41 +0100

    parse json to object

--- src/main/java/com/codecool/shop/CartItem.java
+++ src/main/java/com/codecool/shop/CartItem.java
@@ -1,17 +1,8 @@
 package com.codecool.shop;
 
-import com.codecool.shop.model.ProductCategory;
-import com.codecool.shop.model.Supplier;
-
-import java.util.Currency;
-
 public class CartItem {
     protected String name;
     protected String description;
-    private float defaultPrice;
-    private Currency defaultCurrency;
-    private ProductCategory productCategory;
-    private Supplier supplier;
 
     public String getName() {
         return name;
@@ -19,21 +10,5 @@ public class CartItem {
 
     public String getDescription() {
         return description;
-    }
-
-    public float getDefaultPrice() {
-        return defaultPrice;
-    }
-
-    public Currency getDefaultCurrency() {
-        return defaultCurrency;
-    }
-
-    public ProductCategory getProductCategory() {
-        return productCategory;
-    }
-
-    public Supplier getSupplier() {
-        return supplier;
     }
 }

282d6dd0c7dc14079afea73413003819ef7c0cfb
Barnuss09 <hollbarna@gmail.com>
2020-02-18 15:51:11 +0100

    api post

--- src/main/java/com/codecool/shop/controller/ApiCart.java
+++ src/main/java/com/codecool/shop/controller/ApiCart.java
@@ -6,6 +6,9 @@ import com.codecool.shop.dao.ProductDao;
 import com.codecool.shop.dao.implementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.implementation.ProductDaoMem;
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.model.Product;
+import com.google.gson.Gson;
+import jdk.nashorn.internal.parser.JSONParser;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -14,16 +17,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
-@WebServlet(urlPatterns = {"/"})
-public class ProductController extends HttpServlet {
+
+@WebServlet(urlPatterns = {"/apiCart"})
+public class ApiCart extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        ProductDao productDataStore = ProductDaoMem.getInstance();
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String response=req.getReader().lines().collect(Collectors.joining());
+        Gson gson = new Gson();
+        Product product = gson.fromJson(response, Product.class);
+        System.out.println(product);
+
+        //Object obj = new JSONParser().parse("JSONExample.json");
+
+        /*ProductDao productDataStore = ProductDaoMem.getInstance();
         ProductCategoryDao productCategoryDataStore = ProductCategoryDaoMem.getInstance();
         Cart cart=new Cart();
 
@@ -32,13 +44,8 @@ public class ProductController extends HttpServlet {
         context.setVariable("category", productCategoryDataStore.find(1));
         context.setVariable("products", productDataStore.getBy(productCategoryDataStore.find(1)));
         context.setVariable("itemsInCart",cart.getProductsInCart());
-        // // Alternative setting of the template context
-        // Map<String, Object> params = new HashMap<>();
-        // params.put("category", productCategoryDataStore.find(1));
-        // params.put("products", productDataStore.getBy(productCategoryDataStore.find(1)));
-        // context.setVariables(params);
-        engine.process("product/index.html", context, resp.getWriter());
-    }
 
+        engine.process("product/index.html", context, resp.getWriter());*/
+    }
 
 }

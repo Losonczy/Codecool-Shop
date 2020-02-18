fa171e004eaba450c0a05c29974fc4067225bb58
Barnuss09 <hollbarna@gmail.com>
2020-02-18 11:18:11 +0100

    Cart class

---
+++ src/main/java/com/codecool/shop/Cart.java
@@ -0,0 +1,15 @@
+package com.codecool.shop;
+
+import com.codecool.shop.model.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cart {
+    private List<Product> productsInCart=new ArrayList<Product>();
+
+
+    public void addProduct(Product product){
+        productsInCart.add(product);
+    }
+}

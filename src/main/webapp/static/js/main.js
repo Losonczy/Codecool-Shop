3146919fe586ca6b9057638779187926e6045d9e
Evelin Szabados <szabadosevelin7@gmail.com>
2020-02-19 13:28:12 +0100

    Set focus of selected filters

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -4,47 +4,73 @@ function post(url, data) {
         method: 'POST',
         credentials: 'same-origin',
         headers: {
-            'Content-Type': 'application/json'},
+            'Content-Type': 'application/json'
+        },
 
         body: JSON.stringify(data)
     })
 }
 
 function filter() {
+
     const filterButtons = document.querySelectorAll('.category');
     for (let i = 0; i < filterButtons.length; i++) {
         filterButtons[i].addEventListener('click', function () {
-            if (filterButtons[i].textContent === "Animal") {
-                hide("Animal")
-            } else if (filterButtons[i].textContent === "Holiday") {
-                hide("Holiday")
-            } else if (filterButtons[i].textContent === "Cartoon") {
-                hide("Cartoon")
-            } else if (filterButtons[i].textContent === "Other") {
-                hide("Other")
+            let focusSwitch = filterButtons[i].getAttribute("data-switch");
+            setFocus(this);
+            if(focusSwitch == "1"){
+                filterButtons[i].setAttribute('data-switch', "2");
             }else{
-                hide("All")
+                filterButtons[i].setAttribute('data-switch', "1");
             }
-    });
+            if (filterButtons[i].textContent === "Animal") {
+                hide("Animal",focusSwitch)
+            } else if (filterButtons[i].textContent === "Holiday") {
+                hide("Holiday",focusSwitch)
+            } else if (filterButtons[i].textContent === "Cartoon") {
+                hide("Cartoon",focusSwitch)
+            } else if (filterButtons[i].textContent === "Other") {
+                hide("Other",focusSwitch)
+            } else {
+                hide("All",focusSwitch)
+            }
+
+        });
     }
 
 }
 
-function hide(category){
+function hide(category,focusSwitch) {
     const cards = document.querySelectorAll("[data-label]");
 
-    for(let j=0;j<cards.length;j++){
-        cards[j].setAttribute('class','card');
-        cards[j].parentElement.setAttribute('class','col col-sm-12 col-md-6 col-lg-4');
-        if(cards[j].getAttribute("data-label").toString() !== category){
+    for (let j = 0; j < cards.length; j++) {
+
+        show(cards[j]);
+
+        if (cards[j].getAttribute("data-label").toString() !== category) {
             cards[j].setAttribute('class', 'card card-hidden');
-            cards[j].parentElement.setAttribute('class','card-hidden');
-        }if(category === "All"){
-            cards[j].setAttribute('class','card');
-            cards[j].parentElement.setAttribute('class','col col-sm-12 col-md-6 col-lg-4');
+            cards[j].parentElement.setAttribute('class', 'card-hidden');
+        }
+        if (category === "All") {
+            show(cards[j]);
+
+        }
+        if(focusSwitch == "2"){
+            show(cards[j]);
 
         }
     }
+}
+
+function show(card) {
+    card.setAttribute('class', 'card');
+    card.parentElement.setAttribute('class', 'col col-sm-12 col-md-6 col-lg-4');
+}
+
+function setFocus(button){
+    let current = document.getElementsByClassName('focus');
+    current[0].className = current[0].className.replace(" focus", "");
+    button.className += " focus";
 }
 
 filter();
